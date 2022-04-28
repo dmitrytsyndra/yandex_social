9ab6e4f9177fa19d7c8f22ac7c5afd6c7c243ce7
dmitrytsyndra <36396293+dmitrytsyndra@users.noreply.github.com>
2022-04-28 03:29:45 +0300

    Update bot_run.bat

--- bot_run.bat
+++ bot_run.bat
@@ -4,7 +4,7 @@ call $~dp0tbot2804\venv\Scripts\activate
 
 cd  $~dp0tbot2804
 
-set TOKEN=5034555720:AAH6d1zNkNOQzTw5Jp8iVXW93hUy1REOb3g
+set TOKEN="ваш токен"
 
 python tbot_2804\bot_tg_yandex.py
 
